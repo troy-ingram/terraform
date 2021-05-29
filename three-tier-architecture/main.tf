e8e6bbc4e645be1c2ec70688dbaad84f7b4b93de
Troy Ingram <troyingram86@gmail.com>
2021-05-29 09:09:39 -0400

    Add database and variables

--- three-tier-architecture/main.tf
+++ three-tier-architecture/main.tf
@@ -134,31 +134,29 @@ resource "aws_route_table_association" "b" {
 
 #Create EC2 Instance
 resource "aws_instance" "webserver1" {
-  count                  = 1
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1a"
-  vpc_security_group_ids = [aws_security_group.application-sg.id]
-  subnet_id              = aws_subnet.application-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-1.id
   user_data              = file("install_apache.sh")
 
   tags = {
-    Name = "Application Server"
+    Name = "Web Server"
   }
 
 }
 
 resource "aws_instance" "webserver2" {
-  count                  = 1
   ami                    = "ami-0d5eff06f840b45e9"
   instance_type          = "t2.micro"
   availability_zone      = "us-east-1b"
-  vpc_security_group_ids = [aws_security_group.application-sg.id]
-  subnet_id              = aws_subnet.application-subnet-2.id
+  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
+  subnet_id              = aws_subnet.web-subnet-2.id
   user_data              = file("install_apache.sh")
 
   tags = {
-    Name = "Application Server"
+    Name = "Web Server"
   }
 
 }
@@ -190,9 +188,9 @@ resource "aws_security_group" "web-sg" {
 }
 
 # Create Application Security Group
-resource "aws_security_group" "application-sg" {
-  name        = "Application-SG"
-  description = "Allow inbound traffic from web layer"
+resource "aws_security_group" "webserver-sg" {
+  name        = "Webserver-SG"
+  description = "Allow inbound traffic from ALB"
   vpc_id      = aws_vpc.my-vpc.id
 
   ingress {
@@ -211,7 +209,7 @@ resource "aws_security_group" "application-sg" {
   }
 
   tags = {
-    Name = "Application-SG"
+    Name = "Webserver-SG"
   }
 }
 
@@ -226,7 +224,7 @@ resource "aws_security_group" "database-sg" {
     from_port       = 3306
     to_port         = 3306
     protocol        = "tcp"
-    security_groups = [aws_security_group.application-sg.id]
+    security_groups = [aws_security_group.webserver-sg.id]
   }
 
   egress {
@@ -247,4 +245,70 @@ resource "aws_lb" "external-elb" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web-sg.id]
   subnets            = [aws_subnet.web-subnet-1.id, aws_subnet.web-subnet-2.id]
+}
+
+resource "aws_lb_target_group" "external-elb" {
+  name     = "ALB-TG"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.my-vpc.id
+}
+
+resource "aws_lb_target_group_attachment" "external-elb1" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.webserver1.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.webserver1,
+  ]
+}
+
+resource "aws_lb_target_group_attachment" "external-elb2" {
+  target_group_arn = aws_lb_target_group.external-elb.arn
+  target_id        = aws_instance.webserver2.id
+  port             = 80
+
+  depends_on = [
+    aws_instance.webserver2,
+  ]
+}
+
+resource "aws_lb_listener" "external-elb" {
+  load_balancer_arn = aws_lb.external-elb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.external-elb.arn
+  }
+}
+
+resource "aws_db_instance" "default" {
+  allocated_storage      = 10
+  db_subnet_group_name   = aws_db_subnet_group.default.id
+  engine                 = "mysql"
+  engine_version         = "8.0.20"
+  instance_class         = "db.t2.micro"
+  multi_az               = true
+  name                   = "mydb"
+  username               = "username"
+  password               = "password"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.database-sg.id]
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "main"
+  subnet_ids = [aws_subnet.database-subnet-1.id, aws_subnet.database-subnet-2.id]
+
+  tags = {
+    Name = "My DB subnet group"
+  }
+}
+
+output "lb_dns_name" {
+  description = "The DNS name of the load balancer"
+  value       = aws_lb.external-elb.dns_name
 }
